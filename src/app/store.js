f1e8117e23107eef77c7b9ef37aa3131f8f9d7eb
Nguyễn Anh Nhựt <55295665+Nanz99@users.noreply.github.com>
2022-07-16 00:09:36 -0700

    aa

--- src/app/store.js
+++ src/app/store.js
@@ -3,6 +3,10 @@ import CartReducer from "features/Cart/CartSlice"
 import filtersReducer from "features/Products/filtersSlice"
 import productsReducer from "features/Products/productsSlice"
 import usersReducer  from 'features/Auth/userSlice';
+
+
+
+
 export const store = configureStore({
    reducer: {
       products: productsReducer,
